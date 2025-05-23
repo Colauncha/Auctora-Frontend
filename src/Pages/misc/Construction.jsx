cba4224288c408eceed889bad0c363c1e5cabe2e
I-yan-u <iyanuajimobi12@gmail.com>
2025-05-23 14:13:21 +0100

    Updates for contact us page

--- src/Pages/misc/Construction.jsx
+++ src/Pages/misc/Construction.jsx
@@ -1,34 +1,24 @@
 import { Link } from 'react-router-dom';
+import { Construction } from '../../Constants';
 
 const NotFound = () => {
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 flex flex-col items-center justify-center p-6 mb-20">
       <div className="max-w-md w-full bg-white rounded-xl shadow-lg overflow-hidden p-8 text-center">
         <div className="mx-auto w-32 h-32 rounded-full bg-gradient-to-br from-red-100 to-red-200 flex items-center justify-center mb-8">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-20 w-20 text-red-500"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fillRule="evenodd"
-              d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
-              clipRule="evenodd"
-            />
-          </svg>
+          <img className="w-28 h-28" src={Construction} alt="" />
         </div>
-
+        {/* 
         <h1 className="text-3xl font-extrabold text-black-800 mb-2">
-          404 Error
-        </h1>
+          Page 
+        </h1> */}
         <h2 className="text-xl font-semibold text-gray-800 mb-4">
-          Page Not Found
+          Page under construction
         </h2>
 
         <p className="text-gray-500 mb-8">
-          Oops! The page you&apos;re looking for doesn&apos;t exist or has been
-          removed. Please check the URL or go back to our homepage.
+          Oops! The page you&apos;re looking is currently under construction.
+          Please check back some other time.
         </p>
 
         <Link
