c66aff53ebb8c445b79a1c2c2400b3a47a989ad7
I-yan-u <iyanuajimobi12@gmail.com>
2026-07-31 04:13:46 +0100

    Fixed responsiveness on add product screens

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-bbf79a78'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.kp7iud66t5"
+    "revision": "0.nqh8qm8l6n8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
